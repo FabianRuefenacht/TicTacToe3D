3aeda65c6189362dbd264cebcb757f1ffc2bd37c
FabianRuefenacht <f.ruefenacht@outlook.com>
2023-03-10 13:25:59 +0100

    Colors & Positioning

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -4,9 +4,10 @@ import React, { useState } from 'react';
 function App() {
   /*defining states*/
   const [currentPlayer, setCurrentPlayer] = useState('X');
-  const [squares] = useState(Array(64).fill(''));
+  const [squares, setSquares] = useState(Array(64).fill(''));
   const [winner, setWinner] = useState(null);
   const [tie, setTie] = useState(false);
+  const [show3D, setShow3D] = useState(false);
   const Tiechecker = Array(0)
 
   /*What happens after Clicking a Tile*/
@@ -208,8 +209,16 @@ function App() {
     }
   }
 
+  function restart() {
+    setSquares(Array(64).fill(''));
+    setCurrentPlayer('X');
+    setWinner(null);
+    setTie(false);
+  }
+
   return (
     <div className="App">
+      <button onClick={() => setShow3D(!show3D)}>{show3D === false ? "show 3D View" : "show 2D View"}</button>
       {tie === true &&
         <div>It's a tie.</div>
       }
@@ -218,110 +227,111 @@ function App() {
       }
       {winner !== null && tie === false &&
         <div>{currentPlayer} won!</div>}
-      <div className='layer'>
+      <div className={show3D === false ? 'layer' : 'layer3D'}>
         <div className='row'>
-          <div className='cell' onClick={() => handleClick(0)}>{squares[0]}</div>
-          <div className='cell' onClick={() => handleClick(1)}>{squares[1]}</div>
-          <div className='cell' onClick={() => handleClick(2)}>{squares[2]}</div>
-          <div className='cell' onClick={() => handleClick(3)}>{squares[3]}</div>
+          <div className={squares[0] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(0)}>{squares[0]}</div>
+          <div className={squares[1] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(1)}>{squares[1]}</div>
+          <div className={squares[2] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(2)}>{squares[2]}</div>
+          <div className={squares[3] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(3)}>{squares[3]}</div>
         </div>
         <div className='row'>
-          <div className='cell' onClick={() => handleClick(4)}>{squares[4]}</div>
-          <div className='cell' onClick={() => handleClick(5)}>{squares[5]}</div>
-          <div className='cell' onClick={() => handleClick(6)}>{squares[6]}</div>
-          <div className='cell' onClick={() => handleClick(7)}>{squares[7]}</div>
+          <div className={squares[4] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(4)}>{squares[4]}</div>
+          <div className={squares[5] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(5)}>{squares[5]}</div>
+          <div className={squares[6] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(6)}>{squares[6]}</div>
+          <div className={squares[7] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(7)}>{squares[7]}</div>
         </div>
         <div className='row'>
-          <div className='cell' onClick={() => handleClick(8)}>{squares[8]}</div>
-          <div className='cell' onClick={() => handleClick(9)}>{squares[9]}</div>
-          <div className='cell' onClick={() => handleClick(10)}>{squares[10]}</div>
-          <div className='cell' onClick={() => handleClick(11)}>{squares[11]}</div>
+          <div className={squares[8] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(8)}>{squares[8]}</div>
+          <div className={squares[9] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(9)}>{squares[9]}</div>
+          <div className={squares[10] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(10)}>{squares[10]}</div>
+          <div className={squares[11] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(11)}>{squares[11]}</div>
         </div>
         <div className='row'>
-          <div className='cell' onClick={() => handleClick(12)}>{squares[12]}</div>
-          <div className='cell' onClick={() => handleClick(13)}>{squares[13]}</div>
-          <div className='cell' onClick={() => handleClick(14)}>{squares[14]}</div>
-          <div className='cell' onClick={() => handleClick(15)}>{squares[15]}</div>
-        </div>
-      </div>
-      <div className='layer'>
-        <div className='row'>
-          <div className='cell' onClick={() => handleClick(16)}>{squares[16]}</div>
-          <div className='cell' onClick={() => handleClick(17)}>{squares[17]}</div>
-          <div className='cell' onClick={() => handleClick(18)}>{squares[18]}</div>
-          <div className='cell' onClick={() => handleClick(19)}>{squares[19]}</div>
-        </div>
-        <div className='row'>
-          <div className='cell' onClick={() => handleClick(20)}>{squares[20]}</div>
-          <div className='cell' onClick={() => handleClick(21)}>{squares[21]}</div>
-          <div className='cell' onClick={() => handleClick(22)}>{squares[22]}</div>
-          <div className='cell' onClick={() => handleClick(23)}>{squares[23]}</div>
-        </div>
-        <div className='row'>
-          <div className='cell' onClick={() => handleClick(24)}>{squares[24]}</div>
-          <div className='cell' onClick={() => handleClick(25)}>{squares[25]}</div>
-          <div className='cell' onClick={() => handleClick(26)}>{squares[26]}</div>
-          <div className='cell' onClick={() => handleClick(27)}>{squares[27]}</div>
-        </div>
-        <div className='row'>
-          <div className='cell' onClick={() => handleClick(28)}>{squares[28]}</div>
-          <div className='cell' onClick={() => handleClick(29)}>{squares[29]}</div>
-          <div className='cell' onClick={() => handleClick(30)}>{squares[30]}</div>
-          <div className='cell' onClick={() => handleClick(31)}>{squares[31]}</div>
+          <div className={squares[12] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(12)}>{squares[12]}</div>
+          <div className={squares[13] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(13)}>{squares[13]}</div>
+          <div className={squares[14] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(14)}>{squares[14]}</div>
+          <div className={squares[15] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(15)}>{squares[15]}</div>
         </div>
       </div>
-      <div className='layer'>
+      <div className={show3D === false ? 'layer' : 'layer3D'}>
         <div className='row'>
-          <div className='cell' onClick={() => handleClick(32)}>{squares[32]}</div>
-          <div className='cell' onClick={() => handleClick(33)}>{squares[33]}</div>
-          <div className='cell' onClick={() => handleClick(34)}>{squares[34]}</div>
-          <div className='cell' onClick={() => handleClick(35)}>{squares[35]}</div>
+          <div className={squares[16] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(16)}>{squares[16]}</div>
+          <div className={squares[17] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(17)}>{squares[17]}</div>
+          <div className={squares[18] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(18)}>{squares[18]}</div>
+          <div className={squares[19] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(19)}>{squares[19]}</div>
         </div>
         <div className='row'>
-          <div className='cell' onClick={() => handleClick(36)}>{squares[36]}</div>
-          <div className='cell' onClick={() => handleClick(37)}>{squares[37]}</div>
-          <div className='cell' onClick={() => handleClick(38)}>{squares[38]}</div>
-          <div className='cell' onClick={() => handleClick(39)}>{squares[39]}</div>
+          <div className={squares[20] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(20)}>{squares[20]}</div>
+          <div className={squares[21] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(21)}>{squares[21]}</div>
+          <div className={squares[22] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(22)}>{squares[22]}</div>
+          <div className={squares[23] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(23)}>{squares[23]}</div>
         </div>
         <div className='row'>
-          <div className='cell' onClick={() => handleClick(40)}>{squares[40]}</div>
-          <div className='cell' onClick={() => handleClick(41)}>{squares[41]}</div>
-          <div className='cell' onClick={() => handleClick(42)}>{squares[42]}</div>
-          <div className='cell' onClick={() => handleClick(43)}>{squares[43]}</div>
+          <div className={squares[24] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(24)}>{squares[24]}</div>
+          <div className={squares[25] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(25)}>{squares[25]}</div>
+          <div className={squares[26] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(26)}>{squares[26]}</div>
+          <div className={squares[27] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(27)}>{squares[27]}</div>
         </div>
         <div className='row'>
-          <div className='cell' onClick={() => handleClick(44)}>{squares[44]}</div>
-          <div className='cell' onClick={() => handleClick(45)}>{squares[45]}</div>
-          <div className='cell' onClick={() => handleClick(46)}>{squares[46]}</div>
-          <div className='cell' onClick={() => handleClick(47)}>{squares[47]}</div>
-        </div>
-      </div>
-      <div className='layer'>
-        <div className='row'>
-          <div className='cell' onClick={() => handleClick(48)}>{squares[48]}</div>
-          <div className='cell' onClick={() => handleClick(49)}>{squares[49]}</div>
-          <div className='cell' onClick={() => handleClick(50)}>{squares[50]}</div>
-          <div className='cell' onClick={() => handleClick(51)}>{squares[51]}</div>
-        </div>
-        <div className='row'>
-          <div className='cell' onClick={() => handleClick(52)}>{squares[52]}</div>
-          <div className='cell' onClick={() => handleClick(53)}>{squares[53]}</div>
-          <div className='cell' onClick={() => handleClick(54)}>{squares[54]}</div>
-          <div className='cell' onClick={() => handleClick(55)}>{squares[55]}</div>
-        </div>
-        <div className='row'>
-          <div className='cell' onClick={() => handleClick(56)}>{squares[56]}</div>
-          <div className='cell' onClick={() => handleClick(57)}>{squares[57]}</div>
-          <div className='cell' onClick={() => handleClick(58)}>{squares[58]}</div>
-          <div className='cell' onClick={() => handleClick(59)}>{squares[59]}</div>
-        </div>
-        <div className='row'>
-          <div className='cell' onClick={() => handleClick(60)}>{squares[60]}</div>
-          <div className='cell' onClick={() => handleClick(61)}>{squares[61]}</div>
-          <div className='cell' onClick={() => handleClick(62)}>{squares[62]}</div>
-          <div className='cell' onClick={() => handleClick(63)}>{squares[63]}</div>
+          <div className={squares[28] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(28)}>{squares[28]}</div>
+          <div className={squares[29] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(29)}>{squares[29]}</div>
+          <div className={squares[30] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(30)}>{squares[30]}</div>
+          <div className={squares[31] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(31)}>{squares[31]}</div>
         </div>
       </div>
+      <div className={show3D === false ? 'layer' : 'layer3D'}>
+        <div className='row'>
+          <div className={squares[32] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(32)}>{squares[32]}</div>
+          <div className={squares[33] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(33)}>{squares[33]}</div>
+          <div className={squares[34] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(34)}>{squares[34]}</div>
+          <div className={squares[35] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(35)}>{squares[35]}</div>
+        </div>
+        <div className='row'>
+          <div className={squares[36] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(36)}>{squares[36]}</div>
+          <div className={squares[37] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(37)}>{squares[37]}</div>
+          <div className={squares[38] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(38)}>{squares[38]}</div>
+          <div className={squares[39] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(39)}>{squares[39]}</div>
+        </div>
+        <div className='row'>
+          <div className={squares[40] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(40)}>{squares[40]}</div>
+          <div className={squares[41] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(41)}>{squares[41]}</div>
+          <div className={squares[42] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(42)}>{squares[42]}</div>
+          <div className={squares[43] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(43)}>{squares[43]}</div>
+        </div>
+        <div className='row'>
+          <div className={squares[44] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(44)}>{squares[44]}</div>
+          <div className={squares[45] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(45)}>{squares[45]}</div>
+          <div className={squares[46] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(46)}>{squares[46]}</div>
+          <div className={squares[47] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(47)}>{squares[47]}</div>
+        </div>
+      </div>
+      <div className={show3D === false ? 'layer' : 'layer3D'}>
+        <div className='row'>
+          <div className={squares[48] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(48)}>{squares[48]}</div>
+          <div className={squares[49] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(49)}>{squares[49]}</div>
+          <div className={squares[50] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(50)}>{squares[50]}</div>
+          <div className={squares[51] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(51)}>{squares[51]}</div>
+        </div>
+        <div className='row'>
+          <div className={squares[52] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(52)}>{squares[52]}</div>
+          <div className={squares[53] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(53)}>{squares[53]}</div>
+          <div className={squares[54] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(54)}>{squares[54]}</div>
+          <div className={squares[55] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(55)}>{squares[55]}</div>
+        </div>
+        <div className='row'>
+          <div className={squares[56] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(56)}>{squares[56]}</div>
+          <div className={squares[57] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(57)}>{squares[57]}</div>
+          <div className={squares[58] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(58)}>{squares[58]}</div>
+          <div className={squares[59] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(59)}>{squares[59]}</div>
+        </div>
+        <div className='row'>
+          <div className={squares[60] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(60)}>{squares[60]}</div>
+          <div className={squares[61] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(61)}>{squares[61]}</div>
+          <div className={squares[62] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(62)}>{squares[62]}</div>
+          <div className={squares[63] === 'X' ? 'cellX' : 'cell'} onClick={() => handleClick(63)}>{squares[63]}</div>
+        </div>
+      </div>
+      <button onClick={() => restart()} className='restart'>restart game</button>
     </div>
   );
 }
